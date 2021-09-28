b7b1129c2726399b399e21acb9b19652c61d8bb5
Amos Feng <zfeng@redhat.com>
2021-09-28 11:40:40 +0800

    add shardingsphere-jdbc-core dependency (#1)

--- deployment/src/main/java/io/quarkiverse/shardingsphere/jdbc/deployment/ShardingsphereJdbcProcessor.java
+++ deployment/src/main/java/io/quarkiverse/shardingsphere/jdbc/deployment/ShardingsphereJdbcProcessor.java
@@ -1,14 +1,48 @@
 package io.quarkiverse.shardingsphere.jdbc.deployment;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Singleton;
+
+import org.apache.shardingsphere.driver.jdbc.core.datasource.ShardingSphereDataSource;
+import org.jboss.jandex.DotName;
+
+import io.quarkiverse.shardingsphere.jdbc.ShardingsphereJdbcRecorder;
+import io.quarkus.agroal.spi.JdbcDataSourceBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 
 class ShardingsphereJdbcProcessor {
 
     private static final String FEATURE = "shardingsphere-jdbc";
+    private static final DotName DATA_SOURCE = DotName.createSimple(javax.sql.DataSource.class.getName());
 
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.STATIC_INIT)
+    void generateShardingSphereDataSource(ShardingsphereJdbcRecorder recorder,
+            List<JdbcDataSourceBuildItem> dataSourceBuildItems,
+            BuildProducer<SyntheticBeanBuildItem> syntheticBeanBuildItemBuildProducer) {
+        List<String> dataSources = dataSourceBuildItems
+                .stream().filter(ds -> !ds.isDefault()).map(ds -> ds.getName()).collect(Collectors.toList());
+
+        SyntheticBeanBuildItem.ExtendedBeanConfigurator configurator = SyntheticBeanBuildItem
+                .configure(ShardingSphereDataSource.class)
+                .addType(DATA_SOURCE)
+                .scope(Singleton.class)
+                .setRuntimeInit()
+                .unremovable()
+                .supplier(recorder.shardingsphereDataSourceSupplier(dataSources));
+
+        syntheticBeanBuildItemBuildProducer.produce(configurator.done());
     }
 }
--- integration-tests/src/test/java/io/quarkiverse/shardingsphere/jdbc/it/ShardingsphereJdbcResourceTest.java
+++ integration-tests/src/test/java/io/quarkiverse/shardingsphere/jdbc/it/ShardingsphereJdbcResourceTest.java
@@ -5,9 +5,12 @@ import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class ShardingsphereJdbcResourceTest {
 
     @Test
