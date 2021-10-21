8b6a219395db3b6631763e2fe32fc278d32f328b
Amos Feng <zfeng@redhat.com>
2021-10-21 23:02:14 +0800

    Add sharding table rule and test (#6)

--- integration-tests/src/main/java/io/quarkiverse/shardingsphere/jdbc/it/ShardingsphereJdbcResource.java
+++ integration-tests/src/main/java/io/quarkiverse/shardingsphere/jdbc/it/ShardingsphereJdbcResource.java
@@ -1,32 +1,91 @@
-/*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
 package io.quarkiverse.shardingsphere.jdbc.it;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.sql.DataSource;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
 
 @Path("/shardingsphere-jdbc")
 @ApplicationScoped
 public class ShardingsphereJdbcResource {
-    // add some rest methods here
+    @Inject
+    DataSource dataSource;
+
+    @Named("ds")
+    DataSource h2_ds;
+
+    void onStart(@Observes StartupEvent ev) throws Exception {
+        createAccountTable();
+    }
+
+    void onStop(@Observes ShutdownEvent ev) throws Exception {
+        dropAccountTable();
+    }
+
+    private void createAccountTable() throws SQLException {
+        String sql = "CREATE TABLE t_account (account_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (account_id))";
+        try (Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        }
+    }
+
+    private void dropAccountTable() throws SQLException {
+        String sql = "DROP TABLE t_account";
+        try (Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        }
+    }
+
+    @POST
+    @Path("/account")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Integer createAccount(Account account) throws Exception {
+        int result = 0;
+
+        String sql = "INSERT INTO t_account (account_id, user_id, status) VALUES (?, ?, ?)";
+        try (Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, account.getAccount_id());
+            statement.setInt(2, account.getUser_id());
+            statement.setString(3, account.getStatus());
+            result = statement.executeUpdate();
+        }
+
+        return result;
+    }
 
     @GET
-    public String hello() {
-        return "Hello shardingsphere-jdbc";
+    @Path("/account/{tbl}")
+    public Integer count(@PathParam("tbl") String table) throws Exception {
+        String sql = "SELECT COUNT(*) from " + table;
+        try (Connection connection = h2_ds.getConnection();
+                Statement statement = connection.createStatement()) {
+            ResultSet result = statement.executeQuery(sql);
+            if (result.next()) {
+                return result.getInt(1);
+            } else {
+                return -1;
+            }
+        }
     }
 }
--- runtime/src/main/java/io/quarkiverse/shardingsphere/jdbc/ShardingsphereJdbcRecorder.java
+++ runtime/src/main/java/io/quarkiverse/shardingsphere/jdbc/ShardingsphereJdbcRecorder.java
@@ -1,6 +1,8 @@
 package io.quarkiverse.shardingsphere.jdbc;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -10,22 +12,77 @@ import java.util.stream.Collectors;
 import javax.sql.DataSource;
 
 import org.apache.shardingsphere.driver.api.ShardingSphereDataSourceFactory;
+import org.apache.shardingsphere.infra.config.RuleConfiguration;
+import org.apache.shardingsphere.infra.config.algorithm.ShardingSphereAlgorithmConfiguration;
+import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
+import org.apache.shardingsphere.sharding.api.config.rule.ShardingTableRuleConfiguration;
+import org.apache.shardingsphere.sharding.api.config.strategy.keygen.KeyGenerateStrategyConfiguration;
+import org.apache.shardingsphere.sharding.api.config.strategy.sharding.StandardShardingStrategyConfiguration;
 
 import io.quarkus.agroal.runtime.DataSources;
 import io.quarkus.runtime.annotations.Recorder;
+import io.quarkus.runtime.configuration.ConfigurationException;
 
 @Recorder
 public class ShardingsphereJdbcRecorder {
-    public Supplier<DataSource> shardingsphereDataSourceSupplier(List<String> dataSources) {
+    public Supplier<DataSource> shardingsphereDataSourceSupplier(ShardingsphereConfig config, List<String> dataSources) {
         return () -> {
             try {
                 Map<String, DataSource> dataSourceMap = dataSources
                         .stream().collect(Collectors.toMap(s -> s, DataSources::fromName));
 
-                return ShardingSphereDataSourceFactory.createDataSource(dataSourceMap, null, new Properties());
+                return ShardingSphereDataSourceFactory.createDataSource(dataSourceMap, getShardingRulesFromConfig(config),
+                        new Properties());
             } catch (SQLException e) {
-                return null;
+                throw new ConfigurationException(e);
             }
         };
+    }
+
+    private List<RuleConfiguration> getShardingRulesFromConfig(ShardingsphereConfig config) {
+        List<RuleConfiguration> rules = new ArrayList<>();
+        ShardingsphereConfig.ShardingRulesConfig sharding = config.rules.sharding;
+
+        ShardingRuleConfiguration shardingRuleConfiguration = new ShardingRuleConfiguration();
+        shardingRuleConfiguration.setBindingTableGroups(Arrays.asList(sharding.bindingTables.orElse("").split(",")));
+        shardingRuleConfiguration.setBroadcastTables(Arrays.asList(sharding.broadcastTables.orElse("").split(",")));
+
+        for (Map.Entry<String, ShardingsphereConfig.ShardingTablesRuleConfig> table : sharding.tables.namedTables.entrySet()) {
+            ShardingTableRuleConfiguration tableRule = new ShardingTableRuleConfiguration(table.getKey(),
+                    table.getValue().actual_data_nodes);
+            ShardingsphereConfig.StandardShardingStrategyConfig shardingStrategyConfig = table
+                    .getValue().tableStrategy.standard;
+            StandardShardingStrategyConfiguration standard = new StandardShardingStrategyConfiguration(
+                    shardingStrategyConfig.column.orElse(
+                            sharding.defaultShardingColumn.orElseThrow(
+                                    () -> new ConfigurationException("no sharding column for table " + table.getKey()))),
+                    shardingStrategyConfig.algorithm);
+            tableRule.setTableShardingStrategy(standard);
+            tableRule.setKeyGenerateStrategy(new KeyGenerateStrategyConfiguration(
+                    table.getValue().keyGenerateStrategy.column,
+                    table.getValue().keyGenerateStrategy.keyGeneratorName));
+            shardingRuleConfiguration.getTables().add(tableRule);
+        }
+
+        for (Map.Entry<String, ShardingsphereConfig.ShardingAlgorithmConfig> algorithm : sharding.algorithms.namedAlgorithms
+                .entrySet()) {
+            Properties properties = new Properties();
+            properties.putAll(algorithm.getValue().props);
+            ShardingSphereAlgorithmConfiguration shardingSphereAlgorithm = new ShardingSphereAlgorithmConfiguration(
+                    algorithm.getValue().type, properties);
+            shardingRuleConfiguration.getShardingAlgorithms().put(algorithm.getKey(), shardingSphereAlgorithm);
+        }
+
+        for (Map.Entry<String, ShardingsphereConfig.ShardingAlgorithmConfig> keyGenerator : sharding.keyGenerators.namedKeyGenerators
+                .entrySet()) {
+            Properties properties = new Properties();
+            properties.putAll(keyGenerator.getValue().props);
+            ShardingSphereAlgorithmConfiguration shardingSphereAlgorithm = new ShardingSphereAlgorithmConfiguration(
+                    keyGenerator.getValue().type, properties);
+            shardingRuleConfiguration.getKeyGenerators().put(keyGenerator.getKey(), shardingSphereAlgorithm);
+        }
+
+        rules.add(shardingRuleConfiguration);
+        return rules;
     }
 }
